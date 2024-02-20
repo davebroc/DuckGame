9c5e7cd8f7945369e3b17bb680ad0cf029081bb0
David Brockbank <davebrockbank02@gmail.com>
2024-02-20 19:26:39 +1300

    Menu change. non-functional options menu

--- Menu.gd
+++ Menu.gd
@@ -1,21 +1,14 @@
-extends Node2D
+extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
-
-
-func _on_button_play_pressed():
+func _on_play_pressed():
 	get_tree().change_scene_to_file("res://World.tscn")
 	pass # Replace with function body.
 
+func _on_options_pressed():
+	get_tree().quit()
+	pass # Replace with function body.
 
-func _on_button_quit_pressed():
+func _on_quit_pressed():
 	get_tree().quit()
 	pass # Replace with function body.
